116a9e11cb3a66b0c29a57459f6acaa2113b0673
hhdMrLion <55070348+hhdMrLion@users.noreply.github.com>
2021-09-15 11:03:12 +0800

    '初始化项目'

---
+++ src/components/header/header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <span class="icon"></span>
+    <h1 class="text">SHIDog Music</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header'
+  }
+</script>
+
+<style scoped lang="scss">
+  .header {
+    height: 44px;
+    text-align: center;
+    color: $color-theme;
+    font-size: 0;
+    .icon {
+      display: inline-block;
+      vertical-align: top;
+      margin-top: 6px;
+      width: 30px;
+      height: 32px;
+      margin-right: 9px;
+      @include bg-image('logo');
+      background-size: 30px 32px;
+    }
+    .text {
+      display: inline-block;
+      vertical-align: top;
+      line-height: 44px;
+      font-size: $font-size-large;
+    }
+  }
+</style>
