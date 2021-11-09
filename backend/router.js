3a7b24fb952252a4fc58408fa51c23227dfc8132
xxcfun <echo_dyc@163.com>
2021-11-09 17:15:49 +0800

    '注册排行榜详情接口'

--- backend/router.js
+++ backend/router.js
@@ -124,6 +124,8 @@ function registerRouter(app) {
   registerAlbum(app)
 
   registerTopList(app)
+
+  registerTopDetail(app)
 }
 
 // 注册推荐列表接口路由
@@ -548,6 +550,55 @@ function registerTopList(app) {
           code: ERR_OK,
           result: {
             topList
+          }
+        })
+      } else {
+        res.json(data)
+      }
+    })
+  })
+}
+
+// 注册排行榜详情接口
+function registerTopDetail(app) {
+  app.get('/api/getTopDetail', (req, res) => {
+    const url = 'https://u.y.qq.com/cgi-bin/musics.fcg'
+    const { id, period } = req.query
+
+    const data = JSON.stringify({
+      detail: {
+        module: 'musicToplist.ToplistInfoServer',
+        method: 'GetDetail',
+        param: {
+          topId: Number(id),
+          offset: 0,
+          num: 100,
+          period
+        }
+      },
+      comm: {
+        ct: 24,
+        cv: 0
+      }
+    })
+
+    const randomKey = getRandomVal('getUCGI')
+    const sign = getSecuritySign(data)
+
+    get(url, {
+      sign,
+      '-': randomKey,
+      data
+    }).then((response) => {
+      const data = response.data
+      if (data.code === ERR_OK) {
+        const list = data.detail.data.songInfoList
+        const songList = handleSongList(list)
+
+        res.json({
+          code: ERR_OK,
+          result: {
+            songs: songList
           }
         })
       } else {
