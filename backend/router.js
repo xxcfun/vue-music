c3500bbbc87ad5aba891aefe48ce174022d777c8
xxcfun <echo_dyc@163.com>
2021-11-09 16:21:17 +0800

    '注册排行榜接口'

--- backend/router.js
+++ backend/router.js
@@ -122,6 +122,8 @@ function registerRouter(app) {
   registerLyric(app)
 
   registerAlbum(app)
+
+  registerTopList(app)
 }
 
 // 注册推荐列表接口路由
@@ -492,6 +494,60 @@ function registerAlbum(app) {
           code: ERR_OK,
           result: {
             songs: songList
+          }
+        })
+      } else {
+        res.json(data)
+      }
+    })
+  })
+}
+
+// 注册排行榜接口
+function registerTopList(app) {
+  app.get('/api/getTopList', (req, res) => {
+    const url = 'https://u.y.qq.com/cgi-bin/musics.fcg'
+
+    const data = JSON.stringify({
+      comm: { ct: 24 },
+      toplist: { module: 'musicToplist.ToplistInfoServer', method: 'GetAll', param: {} }
+    })
+
+    const randomKey = getRandomVal('recom')
+    const sign = getSecuritySign(data)
+
+    get(url, {
+      sign,
+      '-': randomKey,
+      data
+    }).then((response) => {
+      const data = response.data
+      if (data.code === ERR_OK) {
+        const topList = []
+        const group = data.toplist.data.group
+
+        group.forEach((item) => {
+          item.toplist.forEach((listItem) => {
+            topList.push({
+              id: listItem.topId,
+              pic: listItem.frontPicUrl,
+              name: listItem.title,
+              period: listItem.period,
+              songList: listItem.song.map((songItem) => {
+                return {
+                  id: songItem.songId,
+                  singerName: songItem.singerName,
+                  songName: songItem.title
+                }
+              })
+            })
+          })
+        })
+
+        res.json({
+          code: ERR_OK,
+          result: {
+            topList
           }
         })
       } else {
