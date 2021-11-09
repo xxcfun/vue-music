f30647c062de026b98e4ffaa769f71feb53f9024
xxcfun <echo_dyc@163.com>
2021-11-09 16:10:23 +0800

    '注册歌单专辑接口'

--- backend/router.js
+++ backend/router.js
@@ -120,6 +120,8 @@ function registerRouter(app) {
   registerSongsUrl(app)
 
   registerLyric(app)
+
+  registerAlbum(app)
 }
 
 // 注册推荐列表接口路由
@@ -445,6 +447,51 @@ function registerLyric(app) {
           code: ERR_OK,
           result: {
             lyric: Base64.decode(data.lyric)
+          }
+        })
+      } else {
+        res.json(data)
+      }
+    })
+  })
+}
+
+// 注册歌单专辑接口
+function registerAlbum(app) {
+  app.get('/api/getAlbum', (req, res) => {
+    const data = {
+      req_0: {
+        module: 'srf_diss_info.DissInfoServer',
+        method: 'CgiGetDiss',
+        param: {
+          disstid: Number(req.query.id),
+          onlysonglist: 1,
+          song_begin: 0,
+          song_num: 100
+        }
+      },
+      comm: {
+        g_tk: token,
+        uin: '0',
+        format: 'json',
+        platform: 'h5'
+      }
+    }
+
+    const sign = getSecuritySign(JSON.stringify(data))
+
+    const url = `https://u.y.qq.com/cgi-bin/musics.fcg?_=${getRandomVal()}&sign=${sign}`
+
+    post(url, data).then((response) => {
+      const data = response.data
+      if (data.code === ERR_OK) {
+        const list = data.req_0.data.songlist
+        const songList = handleSongList(list)
+
+        res.json({
+          code: ERR_OK,
+          result: {
+            songs: songList
           }
         })
       } else {
